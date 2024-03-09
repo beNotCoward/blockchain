4db9225c8dd1794d7f44002e6ba5ac9810db194e
mesut123 <1428034971@qq.com>
2024-03-09 14:18:36 +0800

    包含校验的区块链实现 2024-03-02

--- blockchain-simple/src/main/java/com/lentouqing/blockchain/Block.java
+++ blockchain-simple/src/main/java/com/lentouqing/blockchain/Block.java
@@ -29,6 +29,10 @@ public class Block {
      */
     private long timeStamp;
 
+    /**
+     * 挖矿证明
+     */
+    private int nonce;
 
     public Block(String hash, String previousHash, String data) {
         this.hash = hash;
@@ -39,21 +43,30 @@ public class Block {
     public Block(String data, String previousHash) {
         this.previousHash = previousHash;
         this.data = data;
-        this.timeStamp = new Date().getTime();
+        this.timeStamp = System.currentTimeMillis();
         this.hash = calculateHash();
     }
 
     public String calculateHash() {
         String calculatedhash = HashUtil.applySha256(
                 previousHash +
-                        Long.toString(timeStamp) +
+                        Long.toString(timeStamp) + Integer.toString(nonce) +
                         data);
         return calculatedhash;
     }
 
+    /**
+     * 在比特币网络中所有的网络节点都分享了它们各自的区块链，然而最长的有效区块链是被全网所统一承认的，
+     * 如果有人恶意来篡改之前的数据，然后创建一条更长的区块链并全网发布呈现在网络中，我们该怎么办呢？
+     * 这就涉及到了区块链中另外一个重要的概念工作量证明，这里就不得不提及一下hashcash(参考此文档理解https://www.jianshu.com/p/1971c474ecef)
+     *
+     * @param difficulty 挖矿难度 低的难度比如1和2，普通的电脑基本都可以马上计算出来，我的建议是在4-6之间进行测试，普通电脑大概会花费3秒时间，在莱特币中难度大概围绕在442592左右
+     */
     public void mineBlock(int difficulty) {
         String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        // 要求需以指定hash值信息匹配
         while (!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
             hash = calculateHash();
         }
         System.out.println("Block Mined!!! : " + hash);
--- blockchain-simple/src/main/java/com/lentouqing/blockchain/BlockChainListTest.java
+++ blockchain-simple/src/main/java/com/lentouqing/blockchain/BlockChainListTest.java
@@ -21,4 +21,34 @@ public class BlockChainListTest {
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
         System.out.println(blockchainJson);
     }
+
+    /**
+     * 循环区块链中的所有区块并且比较hash值，
+     * 这个方法用来检查hash值是否是于计算出来的hash值相等，
+     * 同时previousHash值是否和前一个区块的hash值相等。
+     * 或许你会产生如下的疑问，我们就在一个主函数中创建区块链中的区块，所以不存在被修改的可能性，
+     * 但是你要注意的是，区块链中的一个核心概念就是去中心化，
+     * 每一个区块可能是在网络中的某一个节点中产生的，所以很有可能某个节点把自己节点中的数据修改了
+     * ，那么根据上述的理论数据改变会导致整个区块链的破裂，也就是区块链就无效了
+     * @return
+     */
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+            // 防止区块链数据篡改后未更新链表的情况
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current hashes is wrong");
+                return false;
+            }
+            if(!previousBlock.hash.equals(currentBlock.previousHash)){
+                System.out.println("Current hashes is wrong");
+                return false;
+            }
+        }
+        return true;
+    }
 }
